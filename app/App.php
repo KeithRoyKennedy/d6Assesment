173547ffdf2800957e74797c556a427d216ecd1a
Keith Kennedy <keith@ifsight.com>
2026-01-14 08:55:44 +0200

    Initial MVC PHP Project.

---
+++ app/App.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Keith\D6assesment;
+
+class App
+{
+    public function run()
+    {
+        // Basic application entry point
+        // TODO: Implement routing to controllers
+        echo "App is running";
+    }
+}
+?>
---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+// Application configuration
+
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'd6assesment');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Other configurations can be added here
+?>
